cea6cecbdd157eb2d99614ed17ad00e2db6e0d0b
Mesut OZTURK <mesut8311006@gmail.com>
2023-11-30 21:19:27 +0300

    project started to convert typscript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppRouter from 'router/AppRouter'
+import './app.css'
+
+const App = () => {
+
+    return (
+        <div>
+            <AppRouter />
+        </div>
+    )
+}
+
+export default App
